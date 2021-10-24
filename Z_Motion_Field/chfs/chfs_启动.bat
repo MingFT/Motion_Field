f65d19fa5416069a742cb3acfd225b26df989744
大明MingFT <61370854+MingFT@users.noreply.github.com>
2021-10-24 10:36:26 +0800

    Add files via upload

---
+++ Z_Motion_Field/chfs/chfs_启动.bat
@@ -0,0 +1,16 @@
+@echo off
+mode con cols=80 lines=35
+color 07
+
+echo ---------------------------------------------------------
+echo.
+echo 共享路径 已复制到剪切板
+echo.
+echo 通过聊天软件，Ctrl+V把共享路径发送给你的朋友把。
+echo.
+echo 先不要关闭此窗口，等对方下载完，再关闭。
+echo.
+echo ---------------------------------------------------------
+echo.
+echo.
+chfs.exe --file=chfs_设置.ini
